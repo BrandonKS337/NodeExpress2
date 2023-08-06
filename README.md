c7353747190b96c85f22384869a69bdfaf4a88e7
Project V2 <brandonks337@gmail.com>
2023-08-06 12:26:24 -0500

    server.js app established. test local host and test route displayed properly.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NodeExpress2
 this is the read me file for this folder. goal of this repo is to rework the node to db exercise Robert went through with us in class to review what we need to know start to finish GREAT recap very smooth process
+
+
+step 1:
+set up package.json with dependencies and scripts. Establish the needed installs for your node server
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+
+app.use(express.json()) //auto parses json data for us with this line
+
+app.get('/', (req, res) => { //sets up test route to see if app is running 
+    res.json({message: "Hello world, you crazy son of a gun!"})
+})
+
+const PORT = process.env.PORT || 8000 //sets up the port that app will utilize
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port: ${PORT}`)
+})
